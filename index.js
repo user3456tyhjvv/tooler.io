e009aef54bd78cf688628c24d30ce1218737523b
user3456tyhjvv <sammymuigai131@gmail.com>
2025-09-29 13:17:08 +0300

    commited gigatech tracker

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(helmet({
 }));
 app.use(compression());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://localhost:3000','https://www.tiffad.co.ke'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://localhost:3000','https://www.tiffad.co.ke','https://www.gigatechshop.co.ke'],
   credentials: false,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
